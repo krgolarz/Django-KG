994f940a37cedd9f424522c43dba19ef9aca54e2
krgolarz <a@a>
2023-10-01 11:48:07 +0200

    change to db and filter adddition

--- books/models.py
+++ books/models.py
@@ -3,14 +3,18 @@ import csv
 from django.db import models
 
 
+class PublishPlace(models.Model):
+    city = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     publish_date = models.DateField()
     vote_count = models.IntegerField()
     vote_average = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.CharField(max_length=255, null=True)
+    publish_place = models.OneToOneField(PublishPlace, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.title}"
-
-
-
--- movie/models.py
+++ movie/models.py
@@ -2,9 +2,14 @@ import csv
 
 from django.db import models
 
+
+class MovieStatistics(models.Model):
+    vote_count = models.IntegerField()
+    vote_average = models.DecimalField(max_digits=5, decimal_places=2)
+
+
 # Create your models here.
 class Movie(models.Model):
-
     tmdb_id = models.CharField(max_length=15)
     title = models.CharField(max_length=1000)
     cast = models.CharField(max_length=1000)
@@ -16,24 +21,13 @@ class Movie(models.Model):
     genres = models.CharField(max_length=1000)
     production_companies = models.CharField(max_length=1000)
     release_date = models.DateField()
-    vote_count = models.IntegerField()
-    vote_average = models.DecimalField(max_digits=5, decimal_places=2)
+    statistics = models.OneToOneField(MovieStatistics, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title},{self.release_date}'
 
+
 '''makemigrations'''
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    publish_date = models.DateField()
-    vote_count = models.IntegerField()
-    vote_average = models.DecimalField(max_digits=5, decimal_places=2)
-    author = models.CharField(max_length=255,null=True)
-
-    def __str__(self):
-        return f"{self.title}"
 
 '''
 Movie.objects.filter(title__startswith='Jurassic')
